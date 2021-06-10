6c689d9b8d59fc6ce3d0645944dc0a7d7036fd76
Xavier Hazzard <xavier@hazzard.org.uk>
2021-06-10 18:54:23 +0100

    Finished most styling for mobile responsiveness Ipad

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,9 @@
 import Navbar from "../components/navbar";
-import MainBody from "../components/main-page-body";
 
 export default function About() {
     return (
         <>
             <Navbar />
-            <MainBody />
         </>
     );
 }
